6ac7b7eea1a3ecd5fbc152d31d66c2e338b894b2
Hamza <hamzasepal@gmail.com>
2025-11-14 12:20:09 +0500

    initial push

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdio>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
